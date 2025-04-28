14594a44419253d81983d76ff769884cc64c4292
Arafian Hafiz <arafianw@gmail.com>
2025-04-28 13:51:35 +0700

    Update TagihanController.php

--- app/Http/Controllers/TagihanController.php
+++ app/Http/Controllers/TagihanController.php
@@ -11,40 +11,40 @@ class TagihanController extends Controller
 {
     public function index()
     {
-        // try {
+        try {
 
-        // $tagihan = tagihan::paginate(5);
-        // $user = User::all();
-        // $pelanggan = pelanggan::all();
-        // return view('admin.page.tagihan.index')->with([
-        //     'tagihan' => $tagihan,
-        //     'pelanggan' => $pelanggan,
-        //     'user' => $user
-        // ]);
-        $search = request('search');
-        $entries = request('entries', 10);
-        $tagihan = tagihan::with(['pelanggan', 'pelanggan.user'])
-            ->when($search, function ($query) use ($search) {
-                $query->where(function ($q) use ($search) {
-                    $q->whereHas('pelanggan.user', function ($subQuery) use ($search) {
-                        $subQuery->where('name', 'like', "%$search%");
-                    })
-                        ->orWhere('bulan_tahun', 'like', "%$search%")
-                        ->orWhere('status_pembayaran', 'like', "%$search%")
-                        ->orWhere('jatuh_tempo', 'like', "%$search%");
-                });
-            })
-            ->paginate($entries);
-        return view('admin.page.tagihan.index', [
-            'tagihan' => $tagihan,
-            'search' => $search,
-            'entries' => $entries,
-            'pelanggan' => Pelanggan::all(),
-            'user' => User::all()
-        ]);
-        // } catch (\Exception $e) {
-        //     return redirect()->route('error.index')->with('error_message', 'Error: ' . $e->getMessage());
-        // }
+            // $tagihan = tagihan::paginate(5);
+            // $user = User::all();
+            // $pelanggan = pelanggan::all();
+            // return view('admin.page.tagihan.index')->with([
+            //     'tagihan' => $tagihan,
+            //     'pelanggan' => $pelanggan,
+            //     'user' => $user
+            // ]);
+            $search = request('search');
+            $entries = request('entries', 10);
+            $tagihan = tagihan::with(['pelanggan', 'pelanggan.user'])
+                ->when($search, function ($query) use ($search) {
+                    $query->where(function ($q) use ($search) {
+                        $q->whereHas('pelanggan.user', function ($subQuery) use ($search) {
+                            $subQuery->where('name', 'like', "%$search%");
+                        })
+                            ->orWhere('bulan_tahun', 'like', "%$search%")
+                            ->orWhere('status_pembayaran', 'like', "%$search%")
+                            ->orWhere('jatuh_tempo', 'like', "%$search%");
+                    });
+                })
+                ->paginate($entries);
+            return view('admin.page.tagihan.index', [
+                'tagihan' => $tagihan,
+                'search' => $search,
+                'entries' => $entries,
+                'pelanggan' => Pelanggan::all(),
+                'user' => User::all()
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->route('error.index')->with('error_message', 'Error: ' . $e->getMessage());
+        }
     }
 
     public function store(Request $request)
